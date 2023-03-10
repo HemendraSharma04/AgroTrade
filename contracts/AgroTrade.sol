98188f28c408cc5ced892d17d7b15ae9d7d3017d
CodeNinja04 <hemendra.sharma@squadstack.com>
2023-03-10 23:18:33 +0530

    init

---
+++ contracts/AgroTrade.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FarmerMarketplace {
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        uint quantity;
+        address farmer;
+        bool sold;
+    }
+
+    struct ProductBidding {
+        uint id; // bidding id
+        uint prod_id; // product id
+        uint price;
+        address owner;
+    }
+
+    struct Farmer {
+        string name;
+        string phone;
+        address farmer;
+    }
+
+    struct Merchant {
+        string name;
+        string phone;
+        address merchant;
+    }
+
+    mapping(uint => Product) public products;
+    mapping(uint => ProductBidding) public product_bidding;
+
+    Product[] public products_list;
+    ProductBidding[] public product_bidding_list;
+
+    uint public productsCount;
+    uint public product_biddingCount;
+
+    event ProductAdded(
+        uint id,
+        string name,
+        uint price,
+        uint quantity,
+        address farmer
+    );
+    event ProductSold(uint id, address merchant);
+
+    function addProduct(
+        string memory _name,
+        uint _price,
+        uint _quantity
+    ) public {
+        productsCount++;
+        products[productsCount] = Product(
+            productsCount,
+            _name,
+            _price,
+            _quantity,
+            msg.sender,
+            false
+        );
+        products_list.push(products[productsCount]);
+        emit ProductAdded(productsCount, _name, _price, _quantity, msg.sender);
+    }
+
+    // product get bought by merchant directly without bidding
+    function buyProduct(uint _id, uint _quantity) public payable {
+        Product storage product = products[_id];
+
+        require(product.sold == false, "Product is sold out");
+        require(
+            product.id > 0 && product.id <= productsCount,
+            "Invalid product ID"
+        );
+        require(product.quantity > 0, "Product is sold out");
+        require(product.quantity >= _quantity, "Product is sold out");
+        require(msg.value == product.price, "Incorrect payment amount");
+        require(
+            product.farmer != msg.sender,
+            "You cannot buy your own product"
+        );
+
+        product.quantity -= _quantity;
+        if (product.quantity == 0) {
+            product.sold = true;
+        }
+        emit ProductSold(_id, msg.sender);
+
+        payable(product.farmer).transfer(product.price);
+    }
+
+    function setBid(uint _prodid, uint _price) public {
+        Product storage product = products[_prodid];
+
+        require(product.sold == false, "Product is sold out");
+        require(
+            product.id > 0 && product.id <= productsCount,
+            "Invalid product ID"
+        );
+        product_biddingCount++;
+        ProductBidding storage bid = product_bidding[product_biddingCount];
+        bid.id++;
+        bid.owner = msg.sender;
+        bid.price = _price;
+    }
+
+    // farmers sell
+    function sellProduct(uint _id, uint _quantity, uint price) public {}
+}
